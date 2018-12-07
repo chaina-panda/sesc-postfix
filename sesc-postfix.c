82c9e3fca16963d81ed06526e771e2b332599b4b
chaina-panda <37693568+chaina-panda@users.noreply.github.com>
2018-12-07 19:44:11 +0700

    ver.04
    ver.04

--- sesc-postfix.c
+++ sesc-postfix.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include "postfix.h"
 
-int error1(Node **head){
-  if ((*head)==NULL) {
+int error1(Node *head){
+  if (head==NULL) {
     printf("empty");
     printf("\n");
     exit(0);      
   }
 }
 
-int error2(Node **head){
-Node *hea = (*head)->next;
-  if ((*hea)==NULL) {
+int error2(Node *head){
+Node *hea = head->next;
+  if (hea==NULL) {
     printf("empty");
     printf("\n");
     exit(0);      
@@ -28,43 +28,43 @@ int main(void) {
     if (str <= '9' && str >= '0')
       push_front(&head, str);
     if (str == '~') {
-      error1(&head);     
+      error1(head);     
       int x = -(pop_front(&head));
       push_front(&head, x);
     }
     if (str == '+') {
-      error2(&head);      
+      error2(head);      
       int x = pop_front(&head);
       int y = pop_front(&head);
       push_front(&head, x + y);
     }
     if (str == '-') {
-      error2(&head);
+      error2(head);
       int x = pop_front(&head);
       int y = pop_front(&head);
       push_front(&head, x - y);
     }
     if (str == '*') {
-      error2(&head);
+      error2(head);
       int x = pop_front(&head);
       int y = pop_front(&head);
       push_front(&head, x * y);
     }
     if (str == '/') {
-      error2(&head);
+      error2(head);
       int x = pop_front(&head);
       int y = pop_front(&head);
       push_front(&head, x / y);
     }
     if (str == '%') {
-      error2(&head);
+      error2(head);
       int x = pop_front(&head);
       int y = pop_front(&head);
       push_front(&head, x % y);
     }
   }
   
-  if (error1(&head) == 0 || error2(&head) != 0) {
+  if (error1(head) == 0 || error2(head) != 0) {
     printf("empty");
     printf("\n");
     exit(0);
