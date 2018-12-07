2f17c7b65036695a3a571ee932bb66598f200786
chaina-panda <37693568+chaina-panda@users.noreply.github.com>
2018-12-07 21:09:59 +0700

    ver.15
    ver.15

--- sesc-postfix.c
+++ sesc-postfix.c
@@ -4,38 +4,43 @@
 int main(void) {
   Node *head= NULL;
   char str;
-  while(scanf("%c", &str) && str != '\n'){
-    if (str <= '9' && str >= '0')
-      push_front(&head, str);
-    if (str == '~') {    
+  scanf("%c", &str);
+  for( ;str != '\n'; ){
+    if (str <= '9' && str >= '0'){
+      push_front(&head, str - 48);
+    }
+    switch (str){
+    case '~':   
       int x = -(pop_front(&head));
       push_front(&head, x);
-    }
-    if (str == '+') {     
+      break;
+    case '+':     
       int x = pop_front(&head);
       int y = pop_front(&head);
       push_front(&head, x + y);
-    }
-    if (str == '-') {
+      break;
+    case '-':
       int x = pop_front(&head);
       int y = pop_front(&head);
       push_front(&head, x - y);
-    }
-    if (str == '*') {
+      break;
+    case '*':
       int x = pop_front(&head);
       int y = pop_front(&head);
       push_front(&head, x * y);
-    }
-    if (str == '/') {
+      break;
+    case '/':
       int x = pop_front(&head);
       int y = pop_front(&head);
       push_front(&head, x / y);
-    }
-    if (str == '%') {
+      break;
+    case '%':
       int x = pop_front(&head);
       int y = pop_front(&head);
       push_front(&head, x % y);
+      break;
     }
+    scanf("%c", &str);
   }
   printf("%d", pop_front(&head));
   return 0;
